529fd711b981cde264646dfcba2906df08623913
Tobias Koppers <tobias.koppers@googlemail.com>
2012-11-13 12:18:05 +0100

    fixed output

--- bin/enhanced-require.js
+++ bin/enhanced-require.js
@@ -5,7 +5,6 @@ var file = process.argv.splice(2, 1)[0].split("!");
 
 file.push(path.resolve(file.pop()));
 
-console.dir(file);
 require("../lib/require")(process.cwd(), {
 	recursive: true
 })(file.join("!"));
